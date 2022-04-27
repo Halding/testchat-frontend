a0d40fb767fcffe4c75ed1082cb8b6fd7e70f1a4
SvendHalding <72597582+SvendHalding@users.noreply.github.com>
2022-04-27 14:09:42 +0200

    pushed needed files

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {ChatService} from "./shared/chat.service";
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+  message = new FormControl('')
+  messages: string[] =[];
+
+  constructor(private  chatService: ChatService) { }
+
+  ngOnInit(): void {
+    this.chatService.listenForMessages()
+      .subscribe(message => {
+        this.messages.push(message)
+      } )
+  }
+
+
+  sendMessage(): void {
+    console.log(this.message.value);
+    this.chatService.sendMessage(this.message.value);
+  }
+}
